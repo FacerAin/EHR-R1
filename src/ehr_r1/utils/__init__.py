f81789eb961ecae76372f0863906aad34d565b4c
FacerAin <ywsong.dev@kakao.com>
2025-09-03 15:11:50 +0900

    refactor: remove unused EHRSQL prompt functions and class for cleaner codebase

--- src/ehr_r1/utils/__init__.py
+++ src/ehr_r1/utils/__init__.py
@@ -1,11 +1,6 @@
 """Utility modules."""
 
 from .config import ConfigManager, DataConfig, ModelConfig, TrainingConfig
-from .prompts import (
-    EHRSQLPromptTemplate,
-    create_ehrsql_prompt,
-    extract_sql_from_response,
-)
 from .sql_executor import ExecutionAccuracyEvaluator, SQLExecutor
 
 __all__ = [
@@ -13,9 +8,6 @@ __all__ = [
     "DataConfig",
     "ModelConfig",
     "ConfigManager",
-    "EHRSQLPromptTemplate",
-    "create_ehrsql_prompt",
-    "extract_sql_from_response",
     "SQLExecutor",
     "ExecutionAccuracyEvaluator",
 ]
